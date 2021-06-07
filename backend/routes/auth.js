726f6ae3d55e8d624b5e58f5ca3c3aef7b90409d
Wellington Roth <wellingtonrothh@gmail.com>
2021-06-07 10:52:07 -0300

    post login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,6 @@ const bcrypt = require("bcrypt");
 //Register
 router.post("/registrar", async(req, res) => {
   try{
-
     const salt = await bcrypt.genSalt(10);
     const hashedPass = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
@@ -20,6 +19,22 @@ router.post("/registrar", async(req, res) => {
     res.status(500).json(err);
   }
 })
+
 //Login
+router.post("/login", async(req, res) => {
+  try{
+    const user = await User.findOne({ username: req.body.username })
+    !user && res.status(400).json("Credenciais invalidas!")
+
+    const validated = await bcrypt.compare(req.body.password, user.password)
+    !validated && res.status(400).json("Credenciais invalidas!")
+
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  }catch(err) {
+    res.status(500).json(err);
+  }
+})
+
 // 29:55  https://www.youtube.com/watch?v=OML9f6LXUUs
 module.exports = router;
